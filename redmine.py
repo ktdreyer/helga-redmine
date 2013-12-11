ac4c19d12e7d85c46009195de3eaa58b278931f6
Alfredo Deza <alfredo@deza.pe>
2013-12-11 10:38:47 -0500

    fix the match replacement

--- redmine.py
+++ redmine.py
@@ -1,5 +1,9 @@
+import logging
 import requests
 from helga.plugins import match
+from helga import log, settings
+
+logger = log.getLogger(__name__)
 
 
 @match(r'(.*)(issue|ticket|bug)+\s+(#[0-9]+|[0-9]+)($|\s+)', priority=0)
@@ -7,10 +11,10 @@ def redmine(client, channel, nick, message, matches):
     """
     Match possible Redmine tickets, return links and subject info
     """
-    ticket_number = matches[-2].replace('#', '')
+    ticket_number = matches[0][-2].replace('#', '')
 
     ticket_url = settings.REDMINE_URL % {'ticket': ticket_number}
-    api_url = "%s.json" % url
+    api_url = "%s.json" % ticket_url
     result = requests.get(api_url).json()
 
     try:
@@ -18,5 +22,4 @@ def redmine(client, channel, nick, message, matches):
     except KeyError:
         subject = '[no subject]'
 
-    subject = self.get_subject(ticket_url)
     return "%s might be talking about %s [%s]" % (nick, ticket_url, subject)
