9667423cffbd79b3bff049148583802274e04923
Alfredo Deza <alfredo@deza.pe>
2013-12-11 10:19:31 -0500

    use new decorator API for redmine plugin

--- redmine.py
+++ redmine.py
@@ -1,99 +1,22 @@
 import requests
-import random
-
-import smokesignal
-
-from helga import settings
-from helga.db import db
-from helga.extensions.base import (CommandExtension,
-                                   ContextualExtension)
-from helga.log import setup_logger
+from helga.plugins import match
 
 
-logger = setup_logger(__name__)
+@match(r'(.*)(issue|ticket|bug)+\s+(#[0-9]+|[0-9]+)($|\s+)', priority=0)
+def redmine(client, channel, nick, message, matches):
+    """
+    Match possible Redmine tickets, return links and subject info
+    """
+    ticket_number = matches[-2].replace('#', '')
 
+    ticket_url = settings.REDMINE_URL % {'ticket': ticket_number}
+    api_url = "%s.json" % url
+    result = requests.get(api_url).json()
 
-class RedmineExtension(CommandExtension, ContextualExtension):
-
-    NAME = 'redmine'
-
-    usage = ''
-
-    allow_many = True
-
-    def __init__(self, *args, **kwargs):
-        self.redmine_pats = set()
-
-        # Hack for le instance callbacks
-        @smokesignal.on('signon')
-        def callback():
-            if db is not None:
-                self._init_patterns()
-
-        super(RedmineExtension, self).__init__(*args, **kwargs)
-
-    @property
-    def context(self):
-        # This should not look for URLs. Optionally match url type
-        return r'(.*)(issue|ticket|bug)+\s+(#[0-9]+|[0-9]+)($|\s+)'
-
-    def _init_patterns(self):
-        return
-
-    def transform_match(self, match):
-        ticket_number = match[-2].replace('#', '')
-
-        ticket_url = settings.REDMINE_URL % {'ticket': ticket_number}
-        subject = self.get_subject(ticket_url)
-        return "%s [%s]" % (ticket_url, subject)
-
-    def get_subject(self, url):
-        api_url = "%s.json" % url
-        result = requests.get(api_url).json()
+    try:
+        subject = result['issue']['subject']
+    except KeyError:
         subject = '[no subject]'
-        try:
-            subject = result['issue']['subject']
-        except KeyError:
-            pass
-        return subject
 
-    def handle_message(self, opts, message):
-        if opts['add_re']:
-            message.response = self.add_re(opts['<pattern>'])
-        elif opts['remove_re']:
-            message.response = self.remove_re(opts['<pattern>'])
-
-    def add_re(self, pattern):
-        if pattern not in self.redmine_pats:
-            logger.info('Adding new redmine ticket RE: %s' % pattern)
-
-            self.redmine_pats.add(pattern)
-            re_doc = {'re': pattern}
-
-            # Store in DB
-            if not db.redmine.find(re_doc).count():
-                db.redmine.insert(re_doc)
-        else:
-            logger.info('redmine ticket RE already exists: %s' % pattern)
-
-        return '%(nick)s, ' + random.choice(self.add_acks)
-
-    def remove_re(self, pattern):
-        logger.info('Removing redmine ticket RE: %s' % pattern)
-        self.redmine_pats.discard(pattern)
-        db.redmine.remove({'re': pattern})
-
-        return '%(nick)s, ' + random.choice(self.delete_acks)
-
-    def process(self, message):
-        # Try to contextualize
-        self.contextualize(message)
-
-        if message.has_response:
-            return
-
-        # Try to handle commands
-        opts = self.parse_command(message)
-
-        if self.should_handle_message(opts, message):
-            self.handle_message(opts, message)
+    subject = self.get_subject(ticket_url)
+    return "%s might be talking about %s [%s]" % (nick, ticket_url, subject)
