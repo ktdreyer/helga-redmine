1b4dda203e22f91a7e0c2522c2fec8368b9f9377
Alfredo Deza <alfredo@deza.pe>
2014-10-22 09:19:31 -0400

    use the new helpers that fix garbage punctuation

--- redmine.py
+++ redmine.py
@@ -1,16 +1,39 @@
 import requests
+import re
 from helga.plugins import match
 from helga import log, settings
 
 logger = log.getLogger(__name__)
 
 
-@match(r'(.*)(issue|ticket|bug)+\s+(#[0-9]+|[0-9]+)($|\s+)', priority=0)
+def is_ticket(message):
+   regex = re.compile(
+       r'(.*)(issue|ticket|bug)+\s+(#[0-9]+|[0-9]+)'
+   )
+   return regex.match(message)
+
+
+def sanitize(match):
+    """
+    this function sanitizes the match from a ``regex.match(phrase)``
+    call to return the ticket id only.
+    """
+    if not match:
+        return ''
+    ticket_id = match[-1]  # Always the last one in the group
+    ticket_id = ticket_id.strip()  # probably not necessary?
+    return ticket_id.strip('#')
+
+
+@match(is_ticket, priority=0)
 def redmine(client, channel, nick, message, matches):
     """
     Match possible Redmine tickets, return links and subject info
     """
-    ticket_number = matches[0][-2].replace('#', '')
+    ticket_number = sanitize(matches.groups())
+
+    if not ticket_number:
+        logger.warning('I could not determine the right ticket from matches: %s' % matches.groups())
 
     ticket_url = settings.REDMINE_URL % {'ticket': ticket_number}
     api_url = "%s.json" % ticket_url
